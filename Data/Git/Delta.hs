16bf83cca34cb93ea53800f092a61dee088c1d1a
Vincent Berthoux <vincent.berthoux@lokad.com>
2016-07-04 12:23:35 +0200

    Delta patching optimization

--- Data/Git/Delta.hs
+++ Data/Git/Delta.hs
@@ -5,6 +5,7 @@
 -- Stability   : experimental
 -- Portability : unix
 --
+{-# LANGUAGE BangPatterns #-}
 module Data.Git.Delta
     ( Delta(..)
     , DeltaCmd(..)
@@ -15,6 +16,7 @@ module Data.Git.Delta
 
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Builder as BB
 import Data.Bits
 import Data.Word
 
@@ -22,13 +24,13 @@ import qualified Data.Git.Parser as P
 import           Data.Git.Imports
 
 -- | a delta is a source size, a destination size and a list of delta cmd
-data Delta = Delta Word64 Word64 [DeltaCmd]
+data Delta = Delta !Word64 !Word64 ![DeltaCmd]
     deriving (Show,Eq)
 
 -- | possible commands in a delta
 data DeltaCmd =
-      DeltaCopy ByteString   -- command to insert this bytestring
-    | DeltaSrc Word64 Word64 -- command to copy from source (offset, size)
+      DeltaCopy !ByteString   -- command to insert this bytestring
+    | DeltaSrc !Word64 !Word64 -- command to copy from source (offset, size)
     deriving (Show,Eq)
 
 -- | parse a delta.
@@ -43,27 +45,33 @@ deltaParse = do
     dcmds   <- many (P.anyByte >>= parseWithCmd)
     return $ Delta srcSize resSize dcmds
   where
-        getDeltaHdrSize = unbytes 0 <$> P.vlf
-        -- use a foldl ..
-        unbytes _  []     = 0
-        unbytes sh (x:xs) = (fromIntegral x) `shiftL` sh + unbytes (sh+7) xs
+        getDeltaHdrSize = go 0 0 where
+          go !shift !acc = do
+            !v <- P.anyByte
+            let !acc' = acc .|. (fromIntegral v `clearBit` 7) `unsafeShiftL` shift
+            if v `testBit` 7
+              then go (shift + 7) acc' 
+              else return acc'
+
         -- parse one command, either an extension, a copy from src, or a copy from delta.
+        parseWithCmd 0        = error "delta extension not supported"
         parseWithCmd cmd
-            | cmd == 0        = error "delta extension not supported"
             | cmd `testBit` 7 = do
-                o1 <- word8cond (cmd `testBit` 0) 0
-                o2 <- word8cond (cmd `testBit` 1) 8
-                o3 <- word8cond (cmd `testBit` 2) 16
-                o4 <- word8cond (cmd `testBit` 3) 24
-                s1 <- word8cond (cmd `testBit` 4) 0
-                s2 <- word8cond (cmd `testBit` 5) 8
-                s3 <- word8cond (cmd `testBit` 6) 16
-                let offset = o1 .|. o2 .|. o3 .|. o4
-                let size   = s1 .|. s2 .|. s3
-                return $ DeltaSrc offset (if size == 0 then 0x10000 else size)
+                !o1 <- word8cond (cmd `testBit` 0) 0
+                !o2 <- word8cond (cmd `testBit` 1) 8
+                !o3 <- word8cond (cmd `testBit` 2) 16 
+                !o4 <- word8cond (cmd `testBit` 3) 24
+                !s1 <- word8cond (cmd `testBit` 4) 0
+                !s2 <- word8cond (cmd `testBit` 5) 8
+                !s3 <- word8cond (cmd `testBit` 6) 16 
+                let !offset = o1 .|. o2 .|. o3 .|. o4
+                let !size   = s1 .|. s2 .|. s3
+                return $! DeltaSrc offset (if size == 0 then 0x10000 else size)
             | otherwise       = DeltaCopy <$> P.take (fromIntegral cmd)
-        word8cond cond sh =
-            if cond then (flip shiftL sh . fromIntegral) <$> P.anyByte else return 0
+
+        word8cond False _ = return 0
+        word8cond True sh =
+            (flip unsafeShiftL sh . fromIntegral) <$> P.anyByte
 
 -- | read one delta from a lazy bytestring.
 deltaRead :: [ByteString] -> Maybe Delta
@@ -72,9 +80,11 @@ deltaRead = P.maybeParseChunks deltaParse -- . L.toChunks
 -- | apply a delta on a lazy bytestring, returning a new bytestring.
 deltaApply :: L.ByteString -> Delta -> L.ByteString
 deltaApply src (Delta srcSize _ deltaCmds)
-    | L.length src /= fromIntegral srcSize = error "source size do not match"
-    | otherwise                            = -- FIXME use a bytestring builder here.
-        L.fromChunks $ concatMap resolve deltaCmds
-      where resolve (DeltaSrc o s) = L.toChunks $ takeAt (fromIntegral s) (fromIntegral o) src
-            resolve (DeltaCopy b)  = [b]
-            takeAt sz at = L.take sz . L.drop at
+  | L.length src /= fromIntegral srcSize = error "source size do not match"
+deltaApply src (Delta srcSize _ deltaCmds) = BB.toLazyByteString $ foldMap go deltaCmds where
+  takeAt sz at = L.take sz . L.drop at
+
+  go (DeltaSrc o s) = 
+    BB.lazyByteString $ takeAt (fromIntegral s) (fromIntegral o) src
+  go (DeltaCopy b) = BB.byteString b
+
